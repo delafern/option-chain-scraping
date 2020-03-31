f3115e3b6e1d40f3db801200c4508aca5ca7fef5
Fernando A de la Fuente <60758926+delafern@users.noreply.github.com>
2020-03-30 22:51:30 -0700

    Update c_white.py

--- c_white.py
+++ c_white.py
@@ -13,8 +13,8 @@ from matplotlib import cm
 import matplotlib as mpl
 
 class exp_date():
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,date):
+        self.date = date
         self.strike_list = []
         self.bid_list = []
         self.premium_list = []
@@ -35,7 +35,7 @@ def get_options(response,stock_price,date_str): #removed stock argument from fro
     soup = bs(response.text, "lxml")
     tables = soup.find_all('table')
     if len(tables)<2:
-        print('No data')# for '+date_str)
+        print('No data')
         return None
     table = tables[1]
     rows = table.find_all('tr')  
@@ -43,13 +43,14 @@ def get_options(response,stock_price,date_str): #removed stock argument from fro
     for row in rows[1:]:
         strike_string = row.contents[2].text
         strike = float(strike_string.replace(',',''))
-        bid = row.contents[4].text
-        if bid =='-':
-            bid = 0
-        else:
-            bid = float(bid)
-        new_date.add_vals(strike,bid,(bid/strike))
-    print('Success')    
+        if strike <= stock_price:
+            bid = row.contents[4].text
+            if bid =='-':
+                bid = 0
+            else:
+                bid = float(bid)
+            new_date.add_vals(strike,bid,(bid/strike))
+    print('Successful')    
     return new_date
 
 def get_date():
@@ -67,19 +68,18 @@ def populate_data(date_ux,two_months_ux,stock_price,op_chain): #TODO: remove sto
             response = requests.get(url,headers={'user-agent':'Mozilla/5.0'})
             if response.status_code == 200:
                 date_str = date_dt.strftime("%b_%d_%Y")
-                print('Pulling data for '+op_chain.name+' '+date_str+' -- ',end=''),
+                print('Pulling Option Chain for '+op_chain.name+' '+date_str+' -- ',end=''),
                 newest = get_options(response,stock_price,date_str)
                 if newest != None:
                     op_chain.add_exp_date(newest)
         date_ux+=86400
-#    return op_chain
 
 #TODO:add a menu to get stock tickers
 #get stock ticker
-tickers = 'SPY','GOOG'
+tickers = ['SPY']
 
 #get current stock price right now TODO:remove me when scraping function for stock is good to go.
-stock_price = 253.4
+stock_price = 261
 
 #master list of option chains for tickers
 MASTER = []
@@ -87,7 +87,6 @@ MASTER = []
 for ticker in tickers:
     #set stock class per ticker
     MASTER.append(stock_op_chain(ticker))
-
 
 #get dates
 date_ux,two_months_ux = get_date()
@@ -97,9 +96,9 @@ for TCKR in MASTER:
 
 
 ##TODO: loop through plots
-SPY = MASTER[1]
+SPY = MASTER[0]
 #begin plot stuff
-
+plt.style.use('dark_background')
 fig=plt.figure()
 ax=plt.axes()#(projection='3d')
 n=0
@@ -118,8 +117,8 @@ norm = mpl.colors.Normalize(vmin=minval,vmax=maxval)
 
 for dates in SPY.exp_dates:
     premiums = [i*100 for i in dates.premium_list]
-    im = ax.scatter(n*np.ones(len(premiums)),premiums,c=dates.strike_list,norm=norm ,cmap=cm.coolwarm)
-    xaxis_label.append(dates.name)
+    im = ax.scatter(n*np.ones(len(premiums)),premiums,c=dates.strike_list,norm=norm ,cmap=cm.rainbow_r)
+    xaxis_label.append(dates.date)
     x_tics.append(n)
     n+=1
 
@@ -129,14 +128,17 @@ ax.xaxis.set_ticklabels(xaxis_label)
 plt.xticks(rotation=45)
 
 #ylabel
-ax.set_ylabel('Premium')
-plt.ylabel('% Premium',rotation =0)
+plt.ylabel('% Premium')
 
 #colorbar
-cbar = fig.colorbar(im,ax=ax)
+cbar = fig.colorbar(im,ax=ax,orientation ='horizontal',pad=0.2)
 new = cbar.get_ticks()
-new_labels = [f'{(-1*(100-(i/stock_price*100))):.2f}' +'%, $' + f'{(i):.2f}' for i in new]
-cbar.ax.set_yticklabels(new_labels)
-cbar.ax.set_ylabel('$ % out of Money')
-plt.show()
+new_labels = [f'{(1*(100-(i/stock_price*100))):.2f}' +'% / $' + f'{(i):.2f}' for i in new]
+cbar.ax.set_xticklabels(new_labels)
+cbar.ax.set_xlabel('% Out Of Money / Strike')
 
+#grid
+plt.grid(True,linewidth='.5',linestyle='--',color='lightgray')
+
+plt.title(SPY.name+ ' Option Chain',fontsize = 15)
+plt.show()
